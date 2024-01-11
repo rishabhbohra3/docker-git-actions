679310ba1d3b595dac75a9211500dccfc71768e1
Rishabh Bohra <47074390+rishabhbohra3@users.noreply.github.com>
2024-01-11 17:39:08 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
 
-if [ ! -z $INPUT_USERNAME ];
-then echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
-fi
 
-if [ ! -z $INPUT_DOCKER_NETWORK ];
-then INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
-fi
-
-exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
+exec docker run --rm -it mukkii --mode=cli  --pr_url https://gitlab.com/bitoco/ide-plugins/-/merge_requests/960 review --static_analysis.fb_infer.build_tool=gradle --static_analysis.fb_infer.language=java --bee.path=/automation-platform  --bee.actn_dir=/automation-platform/default_bito_ad/bito_modules --git.access_token=glpat-gzmZQVsH5HLpBEssvP9y --bito_cli.bito.access_key=eyJhbGciOiJIUzI1NiJ9.eyJkYXRhIjoidjFfMTIzMV83ODEzMjFfNTQ4OTE2X1RodSBTZXAgMjEgMTI6MDY6MzEgVVRDIDIwMjMifQ.kx2Y-S78PjhLzBC_UAEE9sVliRWy_y6Sf78FNjjMivw --static_analysis.fb_infer.language=NODE --static_analysis.fb_infer.enabled=False --code_feedback=False --dependency_check.enabled=True --dependency_check.snyk_auth_token=6f597190-da44-4c88-b971-662fcea51fc1
