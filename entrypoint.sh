29beaed90b127c4603675baac1f928d8f3f969d8
Rishabh Bohra <47074390+rishabhbohra3@users.noreply.github.com>
2024-01-11 19:00:56 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@
 # DOCKER_PASSWORD="your_password"
 
 # Log in to Docker Hub or your Docker registry (if necessary)
-docker login -u rishabhbohra3 -p ****
+docker login -u rishabhbohra3 -p 9443277454
 
 # Docker image name and optional tag
 IMAGE_NAME="your-docker-image-name"
@@ -24,4 +24,4 @@ IMAGE_TAG="latest"  # You can change this to the desired image tag
 # docker logout
 
 
-exec docker run git-actions:codereview_v1 --mode=cli  --pr_url https://gitlab.com/bitoco/ide-plugins/-/merge_requests/960 review --static_analysis.fb_infer.build_tool=gradle --static_analysis.fb_infer.language=java --bee.path=/automation-platform  --bee.actn_dir=/automation-platform/default_bito_ad/bito_modules --git.access_token=glpat-gzmZQVsH5HLpBEssvP9y --bito_cli.bito.access_key=eyJhbGciOiJIUzI1NiJ9.eyJkYXRhIjoidjFfMTIzMV83ODEzMjFfNTQ4OTE2X1RodSBTZXAgMjEgMTI6MDY6MzEgVVRDIDIwMjMifQ.kx2Y-S78PjhLzBC_UAEE9sVliRWy_y6Sf78FNjjMivw --static_analysis.fb_infer.language=NODE --static_analysis.fb_infer.enabled=False --code_feedback=False --dependency_check.enabled=True --dependency_check.snyk_auth_token=6f597190-da44-4c88-b971-662fcea51fc1
+exec docker run rishabhbohra3/git-actions:codereview_v1 --mode=cli  --pr_url https://gitlab.com/bitoco/ide-plugins/-/merge_requests/960 review --static_analysis.fb_infer.build_tool=gradle --static_analysis.fb_infer.language=java --bee.path=/automation-platform  --bee.actn_dir=/automation-platform/default_bito_ad/bito_modules --git.access_token=glpat-gzmZQVsH5HLpBEssvP9y --bito_cli.bito.access_key=eyJhbGciOiJIUzI1NiJ9.eyJkYXRhIjoidjFfMTIzMV83ODEzMjFfNTQ4OTE2X1RodSBTZXAgMjEgMTI6MDY6MzEgVVRDIDIwMjMifQ.kx2Y-S78PjhLzBC_UAEE9sVliRWy_y6Sf78FNjjMivw --static_analysis.fb_infer.language=NODE --static_analysis.fb_infer.enabled=False --code_feedback=False --dependency_check.enabled=True --dependency_check.snyk_auth_token=6f597190-da44-4c88-b971-662fcea51fc1
